2dbe20dfe4248bca85ec79624f6af6b9d55af2e8
Anthony Legere <anthonyclegere@gmail.com>
2022-03-11 15:55:11 -0500

    updated game UI

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 small js project done through the Odin Project. Rock Paper Scissors game vs CPU in the console.
 
 3/10/21 - adding UI functionality 
+3/11/21 - UI functionality complete, game is working as intended.  
--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-//Logic   
-  //computer chhoice via math random
-  function compChoice() {
-    let compChoice = Math.random();
-    if (compChoice < 0.34) {
-      compChoice = "rock";
+//computer choice via math random
+function compChoice() {
+  let compChoice = Math.random();
+  if (compChoice < 0.34) {
+    compChoice = "rock";
     } else if (compChoice <= 0.67) {
       compChoice = "paper";
     } else {
@@ -13,46 +11,105 @@
     return compChoice
   }
 
-  //counters 
-  let playerScore=0;
-  let cpuScore=0;
-  let drawScore=0;   
+//counters 
+let playerScore=0;
+let cpuScore=0;
+let drawScore=0;   
 
-  function playRound(userChoice, compChoice) { 
-//rock
-  if (userChoice == "rock") {
-    if (compChoice == "scissors") {
-      return (playerScore=playerScore+1) && `Winner! Rock beats Scissors! Computer:${cpuScore}, Player:${playerScore}` ;
-    } else if (compChoice == "paper") {
-      return (cpuScore=cpuScore+1) && `Sorry! Paper beats rock! Computer:${cpuScore}, Player:${playerScore}`;
-    } else if (compChoice == "rock") {
-      return (drawScore=drawScore+1) && `It's a Draw! Computer:${cpuScore}, Player:${playerScore}`;
+//Game Logic 
+function playRound(userChoice, compChoice) { 
+  //rock
+    if (userChoice == "rock") {
+      if (compChoice == "scissors") {
+        return (playerScore=playerScore+1) && `Computer chose scissors. You Win! Rock beats Scissors! Computer:${cpuScore}, Player:${playerScore}` ;
+      } else if (compChoice == "paper") {
+        return (cpuScore=cpuScore+1) && `Sorry! Computer chose paper. Paper beats rock! Computer:${cpuScore}, Player:${playerScore}`;
+      } else if (compChoice == "rock") {
+        return (drawScore=drawScore+1) && `It's a Draw! Computer:${cpuScore}, Player:${playerScore}`;
+      } 
+    }
+  //paper
+    else if (userChoice == "paper") {
+      if (compChoice == "rock") {
+        return (playerScore=playerScore+1) && `You Win! Computer chose paper. Paper beats rock! Computer:${cpuScore}, Player:${playerScore}`;
+      } else if (compChoice == "scissors") {
+        return (cpuScore=cpuScore+1) && `You Lose! Computer chose scissors. Scissors beats Paper! Computer:${cpuScore}, Player:${playerScore}`;
+      } else if (compChoice == "paper") {
+        return (drawScore=drawScore+1) &&`It's a Draw! Computer:${cpuScore}, Player:${playerScore}`;
+      } 
+    }
+  //scissors
+    else if (userChoice == "scissors") {
+      if (compChoice == "paper") {
+        return (playerScore=playerScore+1) &&`You win! Computer chose paper. Scissors beat paper! Computer:${cpuScore}, Player:${playerScore}`;
+      } else if (userChoice == "scissors" && compChoice == "rock") {
+        return (cpuScore=cpuScore+1) && `You Lose! Computer chose rock. Rock beats scissors Computer:${cpuScore}, Player:${playerScore}`;
+      } else if (userChoice == "scissors" && compChoice == "scissors") {
+        return (drawScore=drawScore+1) && `It's a Draw! Computer:${cpuScore}, Player:${playerScore}`;
+      } 
     } 
-  }
-//paper
-  else if (userChoice == "paper") {
-    if (compChoice == "rock") {
-      return (playerScore=playerScore+1) && `You Win! Paper beats rock! Computer:${cpuScore}, Player:${playerScore}`;
-    } else if (compChoice == "scissors") {
-      return (cpuScore=cpuScore+1) && `You Lose! Scissors beats Paper! Computer:${cpuScore}, Player:${playerScore}`;
-    } else if (compChoice == "paper") {
-      return (drawScore=drawScore+1) &&`It's a Draw! Computer:${cpuScore}, Player:${playerScore}`;
-    } 
-  }
-//scissors
-  else if (userChoice == "scissors") {
-    if (compChoice == "paper") {
-      return (playerScore=playerScore+1) &&`You win! scissors beat paper! Computer:${cpuScore}, Player:${playerScore}`;
-    } else if (userChoice == "scissors" && compChoice == "rock") {
-      return (cpuScore=cpuScore+1) && `You Lose! rock beats scissors Computer:${cpuScore}, Player:${playerScore}`;
-    } else if (userChoice == "scissors" && compChoice == "scissors") {
-      return (drawScore=drawScore+1) && `It's a Draw! Computer:${cpuScore}, Player:${playerScore}`;
-    } 
-  } else {
-    return "That's not a valid choice";
-  }
+  };
+
+//Winning conditions function
+function hideButtons(){
+  rock.style.display="none";
+  paper.style.display="none";
+  scissors.style.display="none";
 }
-console.log(playRound(userChoice(), compChoice()))
+
+const winLoseMessage = document.getElementById("win-lose")
+function winLose(){
+  if (playerScore === 5){
+    winLoseMessage.innerHTML = 'You beat the computer! Try again?';
+    document.querySelector('body').style.backgroundColor="#60b347";
+    hideButtons();
+    document.querySelector('.winCondition').innerHTML = '';
+  } else if (cpuScore === 5) {
+    winLoseMessage.innerHTML = 'Tough luck! Try again?';
+    document.querySelector('body').style.backgroundColor="#A13D2D";
+    document.querySelector('.winCondition').innerHTML = '';
+    hideButtons();
+  };
+} 
+
+//user choice 
+const outcome = document.getElementById("outcome")
+const rock = document.getElementById("rock")
+rock.addEventListener("click", function(){
+  outcome.innerHTML = playRound("rock",compChoice());
+winLose()
+});
+
+const paper = document.getElementById("paper")
+paper.addEventListener("click", function(){
+  outcome.innerHTML = playRound("paper",compChoice());
+winLose()
+});
+
+const scissors = document.getElementById("scissors")
+scissors.addEventListener("click", function(){
+  outcome.innerHTML = playRound("scissors",compChoice());
+winLose()
+});
+
+
+
+
+//reset button 
+const reset = document.getElementById("btn")
+reset.addEventListener("click", function(){
+  playerScore=0;
+  cpuScore=0;
+  drawScore=0; 
+  outcome.innerHTML='';
+  winLoseMessage.innerHTML = ''; 
+  document.querySelector('body').style.backgroundColor ="#FFFFFF"
+  rock.style.display="";
+  paper.style.display="";
+  scissors.style.display="";
+  document.querySelector('.winCondition').innerHTML = 'Best of Five!'
+})
+
 
 /*
 //This is to play game in console without UI
@@ -77,5 +134,6 @@ function game(){
     
 }
 console.log(game())
+console.log(playRound(userChoice(), compChoice()))
 */
 
